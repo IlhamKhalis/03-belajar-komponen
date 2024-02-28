529a42287bee96f9242fed326610c528b5fe7465
Ilham Khalis Putra Agatha <khalisagatha@gmail.com>
2024-02-28 15:34:47 +0700

    W03: Jawaban soal 7

--- src/components/mygallery.tsx
+++ src/components/mygallery.tsx
@@ -1,57 +1,51 @@
 import { getImageUrl } from '@/utils/utils';
 
+interface MyProfileProps {
+  name: string;
+  imageId: string;
+  profession: string;
+  awards: string;
+  discovery: string;
+}
+
+const MyProfile: React.FC<MyProfileProps> = ({ name, imageId, profession, awards, discovery }) => {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li><b>Profesi: </b>{profession}</li>
+        <li><b>Penghargaan: </b>{awards}</li>
+        <li><b>Telah Menemukan: </b>{discovery}</li>
+      </ul>
+    </section>
+  );
+}
+
 export default function MyGallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profesi: </b> 
-            Fisikawan dan kimiawan
-          </li>
-          <li>
-            <b>Penghargaan: 4 </b> 
-            (Penghargaan Nobel Fisika, Penghargaan Nobel Kimia, Medali Davy, Medali Matteucci)
-          </li>
-          <li>
-            <b>Telah Menemukan: </b>
-            polonium (unsur kimia)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profesi: </b> 
-            Ahli Geokimia
-          </li>
-          <li>
-            <b>Penghargaan: 2 </b> 
-            (Penghargaan Miyake Geokimia, Penghargaan Tanaka)
-          </li>
-          <li>
-            <b>Telah Menemukan: </b>
-            sebuah metode untuk mengukur karbon dioksida pada air laut
-          </li>
-        </ul>
-      </section>
+      <MyProfile
+        name="Maria Skłodowska-Curie"
+        imageId="szV5sdG"
+        profession="Fisikawan dan kimiawan"
+        awards="4 (Penghargaan Nobel Fisika, Penghargaan Nobel Kimia, Medali Davy, Medali Matteucci)"
+        discovery="polonium (unsur kimia)"
+      />
+      <MyProfile
+        name="Katsuko Saruhashi"
+        imageId="YfeOqp2"
+        profession="Ahli Geokimia"
+        awards="2 (Penghargaan Miyake Geokimia, Penghargaan Tanaka)"
+        discovery="sebuah metode untuk mengukur karbon dioksida pada air laut"
+      />
     </div>
   );
 }
